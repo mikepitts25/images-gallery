2eb1bcc9a787c6558103b824c39fb720919f9b27
Mike Pitts <mikepitts25@gmail.com>
2024-02-12 21:24:22 -0500

    asdf

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,12 +1,18 @@
-import React from 'react';
-import {Navbar} from 'react-bootstrap';
+import React from "react";
+import { Navbar, Container } from "react-bootstrap";
 
-const Header = ({title}) => {
-    return (
-        <Navbar bg="light" data-bs-theme="light">
-          <Navbar.Brand href="/">{title}</Navbar.Brand>
-      </Navbar>
-    )
-}
+const navbarStyle = {
+  backgroundColor: "lightblue",
+};
+
+const Header = ({ title }) => {
+  return (
+    <Navbar style={navbarStyle} data-bs-theme="light">
+      <Container>
+        <Navbar.Brand href="/">{title}</Navbar.Brand>
+      </Container>
+    </Navbar>
+  );
+};
 
 export default Header;
